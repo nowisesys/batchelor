56e5ecd2d5b6455344caefe4fbcd7f1c683c4ce4
Anders Lövgren <andlov@nowise.se>
2007-11-05 21:24:58 +0000

    Modified download.php to create zip-archive of content inside directory 'result'.

--- source/download.php
+++ source/download.php
@@ -41,23 +41,64 @@ $resdir = sprintf("%s/jobs/%s/%s", CACHE_DIRECTORY, $hostid, $resdir);
 if(!file_exists($resdir)) {
     die("The result directory is missing");
 }
-$file = sprintf("%s/stdout", $resdir);
-if(!file_exists($file)) {
-    die("The result file is missing.");
+
+// 
+// Now create the result zip if missing.
+//
+$zipdir  = "result";
+$zipfile = "result.zip";
+
+chdir($resdir);
+if(!file_exists("result.zip")) {
+    if(extension_loaded("zip") && version_compare(phpversion(), "5.2.0", ">="))  {
+	// 
+	// Use bundled PECL zip extention.
+	//
+	$zip = new ZipArchive();	 
+	if($zip->open($zipfile, ZIPARCHIVE::CREATE)) {
+	    $dir = opendir($zipdir);
+	    if($dir) {
+		while(false !== ($file = readdir($dir))) {
+		    if($file != "." && $file != "..") {
+			$zip->addFile(sprintf("%s/%s", $zipdir, $file));
+		    }
+		}
+	    }
+	}
+	$zip->close();
+    }
+    else {
+	// 
+	// Fallback on external command.
+	//
+	$handle = popen(sprintf(ZIP_FILE_COMMAND, $zipfile, $zipdir), "r");
+	pclose($handle);
+    }
 }
 
-$content = file_get_contents($file);
-
+// 
+// Make sure the archive where created:
 //
-// Hint browser about filename to use for "save as..."
-// 
-header(sprintf("Content-Disposition: attachment; filename=\"%s\"", sprintf("result-job%d.txt", $jobid)));
-header(sprintf("Content-Type: %s", "text/plain"));
-header(sprintf("Content-Length: %d", strlen($content)));
-
-// 
-// Now send the file:
-// 
-print $content;
+if(file_exists($zipfile)) {
+    //
+    // Hint browser about filename to use for "save as..."
+    // 
+    header(sprintf("Content-Disposition: attachment; filename=\"%s\"", sprintf("result-job-%d.zip", $jobid)));
+    header(sprintf("Content-Type: %s", "application/zip"));
+    header(sprintf("Content-Length: %d", filesize($zipfile)));
+    
+    // 
+    // Now send the file:
+    // 
+    readfile($zipfile);
+}
+else {
+    // 
+    // Send error document (is their a better solution when zip-file fails?).
+    // 
+    header("HTTP/1.1 500 Internal Server Error");
+    print("<html><body><h4>HTTP Error 500 - Internal Server Error</h4>Failed create zip archive. Contact the server administrator for further information.</body></html>");
+    exit(1);
+}
 
 ?>
