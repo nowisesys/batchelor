1d4d2a60cab5247812a55e7093a426b32526e195
Anders Lövgren <andlov@nowise.se>
2008-03-18 17:32:51 +0000

    A major rework of the about batchelor section in about.php.

--- source/about.php
+++ source/about.php
@@ -32,21 +32,29 @@ include "../conf/config.inc";
 // 
 function print_about_batchelor()
 {
-    printf("<span id=\"secthead\">About batchelor</span>\n");
-    printf("<p>Batchelor is an batch queue manager meant to allow potentional long ");
-    printf("running jobs to be submitted to an web server and scheduled for later ");
-    printf("execution thru an batch queue. Users (submitters) can monitor the state ");
-    printf("of their submitted jobs (pending, running or finished) and later download ");
-    printf("the result from <a href=\"queue?show=queue\">the queue view</a>.</p>\n");
+    print "<span id=\"secthead\">About batchelor</span>\n";
+    print "<p>This web application is powered by Batchelor, a batch job queue manager written in PHP.</p>\n";
+    print "<p>Using an batch queue manager allows potentional long time ";
+    print "running jobs to be submitted to the web server and scheduled for later ";
+    print "execution by its batch queue. Users (submitters) can monitor the state ";
+    print "of their submitted jobs (pending, running or finished) and later download ";
+    print "the result from <a href=\"queue?show=queue\">the queue view</a>.</p>\n";
+    print "<p><b>Features of Batchelor:</b>\n";
+    print "<ul><li>Easy to install, extend and customize.</li>\n";
+    print "    <li>Template system for changing the user interface.</li>\n";
+    print "    <li>No database is required.</li>";
+    print "    <li>Works with all browsers, even text-based (no javascript required).</li>\n";
+    print "    <li>Supports system wide and personal statistics.</li>\n";
+    print "</ul></p>\n";
     
-    printf("<span id=\"secthead\">Licensing</span>\n");    
-    printf("<p>Batchelor is released under <a href=\"about.php?sect=license\">GNU Public License</a> (GPL) and free ");
-    printf("for anyone to modify or redistribute. See the file COPYING bundled ");
-    printf("together with batchelor. Batchelor is originally developed and copyrighted ");
-    printf("&copy; 2007-2008 by Anders Lövgren.</p>\n");
+    print "<span id=\"secthead\">Licensing</span>\n";    
+    print "<p>Batchelor is released under <a href=\"about.php?sect=license\">GNU Public License</a> (GPL) and free ";
+    print "for anyone to modify or redistribute. See the file COPYING bundled ";
+    print "together with batchelor. Batchelor is originally developed and copyrighted ";
+    print "&copy; 2007-2008 by Anders Lövgren.</p>\n";
 
-    printf("<span id=\"secthead\">Authors</span>\n");    
-    printf("<p>The following people have contributed to batchelor:\n<p>");
+    print "<span id=\"secthead\">Authors</span>\n";    
+    print "<p>The following people have contributed to batchelor:\n</p><p>";
     $authors = "../AUTHORS";
     if(file_exists($authors)) {
 	$fp = fopen($authors, "r");
@@ -67,13 +75,56 @@ function print_about_batchelor()
     else {
 	printf("<b>File %s is missing, check your installation!</b>\n", $authors);
     }
-    printf("</p></p>\n");
+    print "</p>\n";
+
+    print "<span id=\"secthead\">Copyright</span>\n";
+    print "<p>Batchelor is Copyright &copy; 2007-2008 by Anders Lövgren and the ";
+    print "Computing Department at Uppsala Biomedical Centre, Uppsala University.</p>\n";
+        
+    $credits = "../CREDITS";
+    if(file_exists($credits)) {
+	$fp = fopen($credits, "r");
+	if($fp) {
+	    $head = "";
+	    $package = array();
+	    while(($str = fgets($fp))) {
+		$str = trim($str);
+		if(strlen($str)) {
+		    $match = array();
+		    if(preg_match("/^\*\s+(.*?):/", $str, $match)) {
+			$head = $match[1];
+		    }
+		    else if(preg_match("/^(http[s]{0,1}:\/\/.*)/", $str, $match)) {
+			$package[$head][] = $match[1];
+		    }
+		}
+	    }
+	    fclose($fp);
+	    if(count($package)) {
+		print "<span id=\"secthead\">Credits</span>\n";    
+		print "<p>The following other projects have been used in batchelor:\n</p>";
+		foreach($package as $name => $links) {
+		    print "<p><u>$name:</u><ul>\n";
+		    foreach($links as $link) {
+			print "<li><a href=\"$link\">$link</a></li>\n";
+		    }
+		    print "</ul></p>\n";
+		}
+	    }
+	}
+	else {
+	    printf("<b>Failed open %s, check your installation!</b>\n", $authors);
+	}	
+    }
     
-    printf("<span id=\"secthead\">Bug reports</span>\n");    
-    printf("<p>If you think you have found a bug in the batch queue application (batchelor), ");
-    printf("then send them to Anders Lövgren &lt;<a href=\"mailto:lespaul@algonet.se\">lespaul@algonet.se</a>&gt;. ");
-    printf("Include any error message, the relevant lines from the Apache error log and the output ");
-    printf("from check.php (runned from the command line is OK). Use 'batchelor: bug report' as subject line.</p>\n");
+    print "<span id=\"secthead\">Download</span>\n";
+    print "<p>The latest version of Batchelor can be downloaded from <a href=\"http://rosalie.bmc.uu.se/batchelor/\">http://rosalie.bmc.uu.se/batchelor/</a></p>\n";
+    
+    print "<span id=\"secthead\">Bug reports</span>\n";    
+    print "<p>If you think you have found a bug in the batch queue application (batchelor), ";
+    print "then send them to Anders Lövgren &lt;<a href=\"mailto:lespaul@algonet.se\">lespaul@algonet.se</a>&gt;. ";
+    print "Include any error message, the relevant lines from the Apache error log and the output ";
+    print "from check.php (runned from the command line is OK). Use 'batchelor: bug report' as subject line.</p>\n";
 }
 
 // 
@@ -83,10 +134,10 @@ function print_license()
 {
     $license = "../COPYING";
     if(file_exists($license)) {
-	printf("<span id=\"secthead\">GNU Public License</span>\n");
-	printf("<p><pre>\n");
+	print "<span id=\"secthead\">GNU Public License</span>\n";
+	print "<p><pre>\n";
 	include $license;
-	printf("</pre></p>\n");
+	print "</pre></p>\n";
     }
 }
 
@@ -109,10 +160,10 @@ function print_about_page($page, $desc)
 	}
     }
     else {
-    	printf("<span id=\"secthead\">Missing page</span>\n");
+    	print "<span id=\"secthead\">Missing page</span>\n";
 	printf("<p>The about page for '%s' is missing. Please check your installation. If this is \n", $desc);	
 	printf("a new installation, then create a HTML page named '%s' with information \n", $page);
-	printf("about your application or remove this tab menu entry inside about.php.\n");
+	print "about your application or remove this tab menu entry inside about.php.\n";
     }
 }
 
