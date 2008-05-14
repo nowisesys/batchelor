c7b448182e92a4e24fcaadf1f11bb94ce2bab919
Anders Lövgren <andlov@nowise.se>
2008-05-14 08:33:20 +0000

    Clearify that pressing 'delete jobs' in queue view will delete only thoose jobs that currently visible.

--- source/queue.php
+++ source/queue.php
@@ -87,7 +87,7 @@ function show_jobs_table(&$jobs)
     printf("<br>\n");
     printf("<form action=\"delete.php\" method=\"GET\">\n");
     printf("  <input type=\"hidden\" name=\"filter\" value=\"%s\" />\n", $_REQUEST['filter']);
-    printf("  <input type=\"submit\" name=\"multiple\" value=\"Delete Jobs\" title=\"Delete all jobs in this list\" />\n");
+    printf("  <input type=\"submit\" name=\"multiple\" value=\"Delete Jobs\" title=\"Delete all jobs showed in this list\" />\n");
     printf("</form>\n");
 }
 
