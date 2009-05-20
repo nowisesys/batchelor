2b2f65aa21afd66a5d00037322a3647cc88f528a
Anders Lövgren <andlov@nowise.se>
2009-05-20 18:31:07 +0000

    Adding download links for indata and result to the result page.

--- source/details.php
+++ source/details.php
@@ -144,8 +144,14 @@ function print_body()
 	    }
 	}
 	printf("<span id=\"secthead\">More information:</span>\n");
-	printf("<p><a href=\"details.php?jobid=%s&result=%s&data=1\" target=\"_blank\">View indata...</a></p>", 
+	printf("<p><ul>\n");
+	printf("<li><a href=\"details.php?jobid=%s&result=%s&data=1\" target=\"_blank\">View indata</a></li>",
 	       $_REQUEST['jobid'], $_REQUEST['result']);
+	printf("<li><a href=\"download.php?jobid=%s&result=%s&what=indata\">Download indata</a></li>",
+	       $_REQUEST['jobid'], $_REQUEST['result']);
+	printf("<li><a href=\"download.php?jobid=%s&result=%s&what=result\">Download result</a></li>",
+	       $_REQUEST['jobid'], $_REQUEST['result']);
+	printf("</ul></p>\n");
 	
 	chdir($cwd);
     }
