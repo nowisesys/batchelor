1eabb34de39588e3fc1ff42f77a4e86bf04b935f
Anders Lövgren <andlov@nowise.se>
2018-08-26 18:25:36 +0000

    Adding component class for dependency injection in inheriting class.

---
+++ source/Batchelor/System/Component.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * Copyright (C) 2018 Anders Lövgren (Nowise Systems)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Batchelor\System;
+
+use Batchelor\System\Service\Hostid;
+use Batchelor\System\Service\Persistance;
+
+/**
+ * The system component class.
+ * 
+ * Provides dependency injection of services into inheriting classes. By default 
+ * the shared services registry is used as injector, but can be overridden by
+ * passing an argument to constructor or using setInjector().
+ * 
+ * Requested services will be automatic injected as a public property in the
+ * calling class if undefined. Next access using the service name should use the
+ * service  from the public property, thus by-passing the magic get method inside 
+ * the component class.
+ * 
+ * If dynamic upgrading the service injector after any service has been injected,
+ * remember to unset any already injected service properties:
+ * 
+ * <code>
+ * $injector = $this->getInjector();
+ * $services = $injector->getServices();
+ * 
+ * foreach (array_keys($services) as $service) {
+ *      if (property_exists($this, $service)) {
+ *              unset($this->{$service});
+ *      }
+ * }
+ * </code>
+ *
+ * @property-read Hostid $hostid The host ID service.
+ * @property-read Persistance $persistance The data persistance service.
+ * 
+ * @author Anders Lövgren (Nowise Systems)
+ */
+class Component
+{
+
+        /**
+         * The services injector.
+         * @var Services 
+         */
+        private $_services;
+
+        /**
+         * Constructor.
+         * @param Services $services The services injector
+         */
+        public function __construct($services = null)
+        {
+                $this->_services = $services;
+        }
+
+        public function __get($name)
+        {
+                if (!($services = $this->getInjector())) {
+                        return null;
+                }
+                if ($services->hasService($name)) {
+                        return $this->$name = $services->getService($name);
+                }
+        }
+
+        /**
+         * Set service injector.
+         * @param Services $services The services injector
+         */
+        public function setInjector($services)
+        {
+                $this->_services = $services;
+        }
+
+        /**
+         * Get service injector.
+         * @return Services
+         */
+        public function getInjector()
+        {
+                if (isset($this->_services)) {
+                        return $this->_services;
+                } else {
+                        return Services::getInstance();
+                }
+        }
+
+}
