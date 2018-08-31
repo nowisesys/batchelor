72e53b24cb4daf3f82acc6cf1aa5ca703581f177
Anders Lövgren <andlov@nowise.se>
2018-08-31 12:35:07 +0000

    Adding file class example with download support.

---
+++ public/example/directory/sendfile.php
@@ -0,0 +1,22 @@
+<?php
+
+use Batchelor\System\File;
+use UUP\Site\Page\Service\StandardService;
+
+// 
+// Simple download script. For use with file example.
+// 
+
+class SendfilePage extends StandardService
+{
+
+        public function render()
+        {
+                // 
+                // Test sending custom heeaders:
+                // 
+                $file = new File("/tmp/test.txt");
+                $file->sendFile(true, ['X-Batchelor-Content' => 'File download example']);
+        }
+
+}
