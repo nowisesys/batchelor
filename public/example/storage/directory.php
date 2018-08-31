e2ee2298b1d3ee875d9b4a6525e78bab0aea469e
Anders Lövgren <andlov@nowise.se>
2018-08-31 15:02:00 +0000

    Moved examples directory -> storage.

---
+++ public/example/storage/directory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class DirectoryPage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Directory class", "directory.inc");
+        }
+
+}
