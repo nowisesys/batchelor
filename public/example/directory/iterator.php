0970d2eb25e48edeb732e8489793b3c164afbb0c
Anders Lövgren <andlov@nowise.se>
2018-08-31 11:49:23 +0000

    Adding directory class examples (iteration, navigation, path and core).

---
+++ public/example/directory/iterator.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class IteratorPage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Recursive iterator", "iterator.inc");
+        }
+
+}
---
+++ public/example/directory/path.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class PathPage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Path information", "path.inc");
+        }
+
+}
