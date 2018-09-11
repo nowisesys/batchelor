5fbd290118c7214e0b062da29801583122915b2f
Anders Lövgren <andlov@nowise.se>
2018-09-11 10:36:42 +0000

    The passthru backend is a special case and should not be placed along with normal backends.

--- source/Batchelor/Cache/Backend/Extension/Passthru.php
+++ source/Batchelor/Cache/Backend/Extension/Passthru.php
@@ -18,8 +18,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-namespace Batchelor\Cache\Backend;
+namespace Batchelor\Cache\Backend\Extension;
 
+use Batchelor\Cache\Backend\Base;
 use Batchelor\Cache\Storage;
 
 /**
