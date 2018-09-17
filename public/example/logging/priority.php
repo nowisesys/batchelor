4f9f14880418562dfc16ac2371d9f2b372d7b7fa
Anders Lövgren <andlov@nowise.se>
2018-09-17 08:58:25 +0000

    Adding priority logger that log messages to individual files based on message priority.

---
+++ public/example/logging/priority.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class PriorityPage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Priority", "priority.inc");
+        }
+
+}
