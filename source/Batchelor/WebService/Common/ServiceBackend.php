dfa79413c3a266a42bbf58288da961f72be0af76
Anders Lövgren <andlov@nowise.se>
2018-09-19 14:00:53 +0000

    Implementing common service backend to dispatch all operations to the queue service. In practice, this might end up as calls to a local or remote work queue.

--- source/Batchelor/WebService/Common/ServiceBackend.php
+++ source/Batchelor/WebService/Common/ServiceBackend.php
@@ -20,7 +20,9 @@
 
 namespace Batchelor\WebService\Common;
 
+use Batchelor\Queue\WorkDirectory;
 use Batchelor\System\Component;
+use Batchelor\WebService\Types\JobData;
 use Batchelor\WebService\Types\JobIdentity;
 use Batchelor\WebService\Types\JobStatus;
 use Batchelor\WebService\Types\QueuedJob;
@@ -43,61 +45,6 @@ class ServiceBackend extends Component
          */
         const API_VERSION = "2.0";
 
-        public function __construct()
-        {
-                
-        }
-
-        /**
-         * Dequeues an already existing job.
-         * 
-         * @param JobIdentity $job The job identity.
-         * @return bool
-         */
-        public function dequeue(JobIdentity $job)
-        {
-                
-        }
-
-        /**
-         * Queues an job for later execution.
-         * 
-         * @param string $indata The input data.
-         * @return QueuedJob[]
-         */
-        public function enqueue(string $indata)
-        {
-                
-        }
-
-        /**
-         * Read file content.
-         * 
-         * Opens the given file from the job directory associated with the job 
-         * identity object and return its content.
-         * 
-         * @param JobIdentity $job The job identity.
-         * @param string $file The filename.
-         * @return string
-         */
-        public function fopen(JobIdentity $job, string $file)
-        {
-                
-        }
-
-        /**
-         * List all queued jobs.
-         * 
-         * Call this method to return a list of all jobs in the current selected 
-         * batch queue.
-         * 
-         * @return JobIdentity[]
-         */
-        public function opendir()
-        {
-                
-        }
-
         /**
          * This method returns the list of queued jobs.
          * 
@@ -107,61 +54,9 @@ class ServiceBackend extends Component
          */
         public function queue(QueueSortResult $sort, QueueFilterResult $filter)
         {
-                
-        }
-
-        /**
-         * Get a list of all files and directories in the job directory associated 
-         * with the job identity object.
-         * 
-         * @param JobIdentity $job The job identity.
-         * @return string[]
-         */
-        public function readdir(JobIdentity $job)
-        {
-                
-        }
-
-        /**
-         * Suspend (pause) an already running job.
-         * @param JobIdentity $job The job identity.
-         * @return bool
-         */
-        public function suspend(JobIdentity $job)
-        {
-                
-        }
-
-        /**
-         * Resume (continue) an already suspended job.
-         * @param JobIdentity $job The job identity.
-         * @return bool
-         */
-        public function resume(JobIdentity $job)
-        {
-                
-        }
-
-        /**
-         * Get status for single job.
-         * 
-         * @param JobIdentity $job
-         * @return JobStatus
-         */
-        public function stat(JobIdentity $job)
-        {
-                
-        }
-
-        /**
-         * Get an list of jobs enqueued after the given timestamp.
-         * 
-         * @param int $stamp The UNIX timestamp.
-         * @return QueuedJob[]
-         */
-        public function watch(int $stamp)
-        {
-                
+                return $this->queue->listJobs(
+                        $this->hostid->getValue(), $sort, $filter
+                );
         }
 
         /**
@@ -174,19 +69,158 @@ class ServiceBackend extends Component
          * @param string $queue The queue name.
          * @return string The new hostid
          */
-        public function select(string $queue)
+        public function select(string $queue): string
         {
                 $this->hostid->setQueue($queue);
                 return $this->hostid->getValue();
         }
 
         /**
+         * Queues an job for later execution.
+         * 
+         * @param JobData $indata The input data.
+         * @return QueuedJob[]
+         */
+        public function enqueue(JobData $indata)
+        {
+                return $this->queue->addJob(
+                        $this->hostid->getValue(), $indata
+                );
+        }
+
+        /**
+         * Dequeues an already existing job.
+         * 
+         * @param JobIdentity $job The job identity.
+         * @return bool
+         */
+        public function dequeue(JobIdentity $job): bool
+        {
+                return $this->queue->removeJob(
+                        $this->hostid->getValue(), $job
+                );
+        }
+
+        /**
+         * Suspend (pause) an already running job.
+         * @param JobIdentity $job The job identity.
+         * @return bool
+         */
+        public function suspend(JobIdentity $job): bool
+        {
+                return $this->queue->suspendJob(
+                        $this->hostid->getValue(), $job
+                );
+        }
+
+        /**
+         * Resume (continue) an already suspended job.
+         * @param JobIdentity $job The job identity.
+         * @return bool
+         */
+        public function resume(JobIdentity $job): bool
+        {
+                return $this->queue->resumeJob(
+                        $this->hostid->getValue(), $job
+                );
+        }
+
+        /**
+         * Get status for single job.
+         * 
+         * @param JobIdentity $job
+         * @return JobStatus
+         */
+        public function stat(JobIdentity $job): JobStatus
+        {
+                return $this->queue->getStatus(
+                        $this->hostid->getValue(), $job
+                );
+        }
+
+        /**
+         * Get an list of jobs enqueued after the given timestamp.
+         * 
+         * @param int $stamp The UNIX timestamp.
+         * @return QueuedJob[]
+         */
+        public function watch(int $stamp)
+        {
+                $result = [];
+                $queued = $this->queue->listJobs(
+                    $this->hostid->getValue()
+                );
+
+                foreach ($queued as $job) {
+                        if ($job->status->stamp > $stamp) {
+                                $result[] = $job;
+                        }
+                }
+
+                return $result;
+        }
+
+        /**
+         * List all queued jobs.
+         * 
+         * Call this method to return a list of all jobs in the current selected 
+         * batch queue.
+         * 
+         * @return JobIdentity[]
+         */
+        public function opendir()
+        {
+                return $this->getDirectory()
+                        ->getJobs();
+        }
+
+        /**
+         * Get a list of all files and directories in the job directory associated 
+         * with the job identity object.
+         * 
+         * @param JobIdentity $job The job identity.
+         * @return string[]
+         */
+        public function readdir(JobIdentity $job)
+        {
+                return $this->getDirectory()
+                        ->getFiles($job);
+        }
+
+        /**
+         * Read file content.
+         * 
+         * Opens the given file from the job directory associated with the job 
+         * identity object and return its content.
+         * 
+         * @param JobIdentity $job The job identity.
+         * @param string $file The filename.
+         * @return string
+         */
+        public function fopen(JobIdentity $job, string $file): string
+        {
+                return $this->getDirectory()
+                        ->getContent($job, $file);
+        }
+
+        /**
          * Get API version.
          * @return string
          */
-        public function version()
+        public function version(): string
         {
                 return self::API_VERSION;
+        }
+
+        /**
+         * Get work directory.
+         * @return WorkDirectory
+         */
+        private function getDirectory(): WorkDirectory
+        {
+                return $this->queue->getReader(
+                        $this->hostid->getValue()
+                );
         }
 
 }
