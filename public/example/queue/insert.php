e95ebbf8be29ac6fa4858f474fb29ae045622b16
Anders Lövgren <andlov@nowise.se>
2018-10-30 15:04:53 +0000

    Adding insert to queue examples. For inserting multiple jobs at once to be viewed by list/get jobs examples (the add, remove and show examples are cleaning up after them).

---
+++ public/example/queue/insert.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class InsertPage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Insert jobs", "insert.inc");
+        }
+
+}
