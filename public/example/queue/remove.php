34a89b5736caf84e69b3bfe3cdb2d375348d80c6
Anders Lövgren <andlov@nowise.se>
2018-10-30 01:00:51 +0000

    Use common modify.inc for add, remove and status examples.

---
+++ public/example/queue/remove.php
@@ -0,0 +1,13 @@
+<?php
+
+use Batchelor\Web\ExamplePage;
+
+class RemovePage extends ExamplePage
+{
+
+        public function __construct()
+        {
+                parent::__construct("Remove job", "remove.inc");
+        }
+
+}
