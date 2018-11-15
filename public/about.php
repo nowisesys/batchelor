9d66eb7004f52a9ece1be74ab9b107eb12db3057
Anders Lövgren <andlov@nowise.se>
2018-11-15 10:55:26 +0000

    Adding about page.

---
+++ public/about.php
@@ -0,0 +1,42 @@
+<?php
+
+/* 
+ * Copyright (C) 2018 Anders Lövgren (Nowise Systems)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */ 
+
+?>
+
+<h3>About Batchelor</h3>
+<p>
+    This system is built on top of Batchelor, a tool for creating web applications 
+    that needs to start jobs that will run for a long period of time, perhaps hours 
+    or weeks.
+</p>
+<a class="w3-btn w3-blue" href="http://it.bmc.uu.se/andlov/proj/sci/batchelor/" style="min-width: 90px">Project</a>
+
+<p>
+    Batchelor provides job queue management, web services (SOAP/JSON) and responsive 
+    web design for your application. Its job queue supports running jobs in parallell 
+    and comes with command line tools for interacting with the scheduler.
+</p>
+<a class="w3-btn w3-deep-orange" href="api/docs" style="min-width: 90px">Services</a>
+
+<p>
+    Since version 2 it supports building distributed system with multiple remote
+    queues. Using composer it can easily be integrated in your project and its
+    API can be used as building bricks in your project.
+</p>
