6aa3a9ba882d906acc036c30d717b6efffa70305
Anders Lövgren <andlov@nowise.se>
2018-11-20 10:19:40 +0000

    Adding admin directory adding script for fixing directory permissions.

---
+++ admin/fix-permissions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Fix directory permissions. Require filesystem mounted with ACL support.
+#
+# Author: Anders Lövgren
+# Date:   2018-11-12
+
+user=apache
+
+tssdir=$(dirname $0)
+topdir=$(dirname $tssdir)
+
+find $topdir/data -type d | xargs setfacl -d -m u:$user:rwx
+find $topdir/data -type d | xargs setfacl -m u:$user:rwx
+find $topdir/data -type f | xargs setfacl -m u:$user:rw
